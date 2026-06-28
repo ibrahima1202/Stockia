8d373845b73e510c9819195a9e8975a5a07c1f9b
ibrahima1202 <69966422+ibrahima1202@users.noreply.github.com>
2026-06-28 13:43:47 +0000

    Update PWA registration method in main.tsx
    Replaced PWA registration with service worker registration.

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { registerSW } from 'virtual:pwa-register'
 
-registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {
-    console.log('STOCKAM est prêt pour une utilisation hors ligne')
-  },
-})
+// Enregistrement PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {})
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
