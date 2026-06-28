5d5271d0d610b2098620a262dc1c4cb196fd9fa1
ibrahima1202 <69966422+ibrahima1202@users.noreply.github.com>
2026-06-28 13:26:44 +0000

    Register PWA service worker for offline support

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {
+    console.log('STOCKAM est prêt pour une utilisation hors ligne')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
