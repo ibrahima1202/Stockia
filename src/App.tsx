44ac4872d390ef312633745f327d4a53055b194e
ibrahima1202 <69966422+ibrahima1202@users.noreply.github.com>
2026-06-30 14:57:11 +0000

    Add StatsPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import FournisseursPage from '@/pages/FournisseursPage'
 import ProfilePage from '@/pages/ProfilePage'
 import PaymentPage from '@/pages/PaymentPage'
 import SubscriptionPage from '@/pages/SubscriptionPage'
+import StatsPage from '@/pages/StatsPage'
 
 export default function App() {
   return (
@@ -42,6 +43,7 @@ export default function App() {
           <Route path="fournisseurs" element={<FournisseursPage />} />
           <Route path="profile" element={<ProfilePage />} />
           <Route path="subscription" element={<SubscriptionPage />} />
+          <Route path="stats" element={<StatsPage />} />
           <Route
             path="expenses"
             element={
